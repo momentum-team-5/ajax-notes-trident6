0e7fd53ff7d0efe1d803e23b331b7fe20dd4a5a2
trident6 <philip.johnson.it@gmail.com>
2020-09-29 00:06:55 -0400

    created elements

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ document.addEventListener('submit', function(event) {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 todoItem: todoInput,
-                created_at: moment().format()
+                created_at: moment().format('YYYY-MM-DD HH:mm')
             })
         })
         .then(res => res.json())
         .then(data => {
             const todoList = document.querySelector('#todo-list')
-            const todoItemEl = document.createElement('li')
+            const todoItemEl = document.createElement('div')
             todoItemEl.innerText = data.todoItem
             todoList.appendChild(todoItemEl)
         })
@@ -29,8 +29,32 @@ fetch(url)
         const todoList = document.querySelector('#todo-list')
         for (const item of todoData) {
             console.log(item)
-            const todoItemEl = document.createElement('li')
+            const todoItemEl = document.createElement('div')
+            todoItemEl.classList.add('note')
             todoItemEl.innerText = item.todoItem
             todoList.appendChild(todoItemEl)
+            const buttonEl = document.createElement('button')
+            buttonEl.innerHTML = "Delete"
+            todoList.appendChild(buttonEl)
         }
     })
+
+// fetch(url)
+// .then(res => res.json())
+// .then(todoData => {
+//     const todoList = document.querySelector('#todo-list')
+//     for (const item of todoData) {
+//         console.log(item)
+//         const todoItemEl = document.createElement('li')
+//         todoItemEl.innerText = item.todoItem
+//         todoList.appendChild(todoItemEl)
+//     }
+// })
+
+
+
+// function deleteNote() {
+//     let messageDiv = document.getElementById('message')
+//     let messageParaOld = messageDiv.getElementsByTagName('p')[0]
+//     messageDiv.removeChild(messageParaOld)
+// }
