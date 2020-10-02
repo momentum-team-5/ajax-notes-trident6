340e2e780f054f9d534f6357013a2a674fb1940f
trident6 <philip.johnson.it@gmail.com>
2020-10-02 16:04:45 -0400

    attemping to follow

--- main.js
+++ main.js
@@ -1,43 +1,123 @@
 /* globals fetch, moment */
+
 const url = 'http://localhost:3000/notes'
 
 document.addEventListener('submit', function(event) {
     event.preventDefault()
-    const todoInput = document.querySelector('#todo-input').value
-    console.log(todoInput)
+    createEntry()
+    renderEntry()
 
-    fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                todoItem: todoInput,
-                created_at: moment().format('YYYY-MM-DD HH:mm')
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            const todoList = document.querySelector('#todo-list')
-            const todoItemEl = document.createElement('div')
-            todoItemEl.innerText = data.todoItem
-            todoList.appendChild(todoItemEl)
-        })
+    // todoList.addEventListener('click', function(e) {
+    //     if (e.target.matches('.delete')) {
+    //         deleteTodo(e.target.parentElement.dataset.id)
+    //     }
+    // })
+
 })
 
-fetch(url)
-    .then(res => res.json())
-    .then(todoData => {
-        const todoList = document.querySelector('#todo-list')
-        for (const item of todoData) {
-            console.log(item)
-            const todoItemEl = document.createElement('div')
-            todoItemEl.classList.add('note')
-            todoItemEl.innerText = item.todoItem
-            todoList.appendChild(todoItemEl)
-            const buttonEl = document.createElement('button')
-            buttonEl.innerHTML = "Delete"
-            todoList.appendChild(buttonEl)
-        }
-    })
+function createEntry() {
+    const diaryInput = document.querySelector('#diary-input')
+    console.log(diaryInput)
+
+    const requestData = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            diaryItem: diaryInput.value,
+            created_at: moment().format('LLLL')
+        })
+    }
+    fetch(url, requestData)
+        .then(res => res.json())
+        .then(data => {
+            diaryInput.value = ''
+            renderEntry(data)
+
+        })
+}
+
+// function renderEntry(note) {
+//     const entryList = document.querySelector('#entry-list')
+//     const enrtyItemEl = document.createElement('div')
+//     const deleteOption = document.createElement('div')
+//         // enrtyItemEl.dataset.id = notes.id
+//         // enrtyItemEl.id = 'item-${note.id}'
+//     enrtyItemEl.innerText = note.diaryItem
+//         // enrtyItemEl.classList.add('note')
+//     deleteOption.classList.add('delete')
+//     deleteOption.innerText = ('Delete')
+//     entryList.appendChild(enrtyItemEl)
+//     entryList.appendChild(deleteOption)
+// }
+
+
+// function deleteEntry(diaryItemId) {
+//     fetch(url + '/' + diaryItemId, {
+//             method: 'DELETE'
+//         })
+//         .then(res => res.json())
+//         .then(data => {
+//             const itemToRemove = document.querySelector(`#item-${noteId}`)
+//             itemToRemove.remove()
+//         })
+
+
+function renderEntry() {
+    fetch(url)
+        .then(res => res.json())
+        .then(noteData => {
+            const entryList = document.querySelector('#entry-list')
+            for (const note of noteData) {
+                console.log(note)
+                const enrtyItemEl = document.createElement('div')
+                enrtyItemEl.id = note.id
+                enrtyItemEl.classList.add('note')
+                const deleteOption = document.createElement('div')
+                deleteOption.classList.add('delete')
+                deleteOption.innerText = ('Delete')
+                enrtyItemEl.innerText = note.diaryItem
+                entryList.appendChild(enrtyItemEl)
+                entryList.appendChild(deleteOption)
+            }
+        })
+}
+// let div = document.createElement('div')
+// div.textContent = moment(note.datetime).format('MMM D h:mm A')
+// el.appendChild(div)
+
+// div = document.createElement('div')
+// const deleteButton = document.createElement('button')
+// deleteButton.classList.add('f5', 'bg-red', 'white', 'br3', 'ph3', 'pv2', 'mb2', 'white', 'pointer')
+// deleteButton.textContent = 'Delete'
+
+// deleteButton.addEventListener('click', function() {
+//     deleteMoodEntry(notes.id)
+// })
+
+// div.appendChild(deleteButton)
+// el.appendChild(div)
+
+// return el
+// }
+
+
+// OLD
+
+
+// fetch(url)
+//     .then(res => res.json())
+//     .then(todoData => {
+//         const todoList = document.querySelector('#todo-list')
+//         for (const item of todoData) {
+//             console.log(item)
+//             const todoItemEl = document.createElement('div')
+//             todoItemEl.classList.add('note')
+//             todoItemEl.innerText = item.todoItem
+//             todoList.appendChild(todoItemEl)
+
+
+
+
 
 // fetch(url)
 // .then(res => res.json())
@@ -48,13 +128,3 @@ fetch(url)
 //         const todoItemEl = document.createElement('li')
 //         todoItemEl.innerText = item.todoItem
 //         todoList.appendChild(todoItemEl)
-//     }
-// })
-
-
-
-// function deleteNote() {
-//     let messageDiv = document.getElementById('message')
-//     let messageParaOld = messageDiv.getElementsByTagName('p')[0]
-//     messageDiv.removeChild(messageParaOld)
-// }
